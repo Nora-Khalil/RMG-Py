8923fe44c2d00bec462585c2ac62ebb9a6676b82
norak <khalil.nor@northeastern.edu>
2022-06-17 17:17:19 -0400

    updated set_cantera_kinetics in falloff.py

--- rmgpy/cantera.py
+++ rmgpy/cantera.py
@@ -1,6 +1,7 @@
 
 import os
 import yaml
+import cantera as ct 
 
 from rmgpy.chemkin import load_chemkin_file
 from rmgpy.species import Species
@@ -15,6 +16,8 @@ from rmgpy.kinetics.surface import StickingCoefficient
 from rmgpy.util import make_output_subdirectory     #checked
 from datetime import datetime
 from rmgpy.chemkin import get_species_identifier
+
+
 
 
 
@@ -91,6 +94,7 @@ elements:
   atomic-weight: 3.016
 - symbol: X
   atomic-weight: 195.083
+
         '''
         f.write(block4)
         yaml.dump(result_dict, stream=f)
@@ -118,157 +122,51 @@ def get_mech_dict(spcs, rxns, solvent='solvent', solvent_data=None):
          if spc.contains_surface_site():
              is_surface = True
              break
-         if not is_surface:   
-             result_dict = dict()
-             result_dict["species"] = dict()
-             #result_dict["species"][0] = [dict()]
-             result_dict["species"][0] = [obj_to_dict(x, spcs, names=names) for x in spcs]
-             # result_dict["species"][0]["thermo"] = [obj_to_dict(x, spcs, names=names) for x in spcs]
-        #     result_dict["reactions"] = [obj_to_dict(x, spcs, names=names) for x in rxns]
-         #if solvent_data:
-           # result_dict["Solvents"] = [obj_to_dict(solvent_data, spcs, names=names, label=solvent)]
+         if not is_surface: 
+             result_dict = dict()  
+             result_dict["species"]= [obj_to_dict(x, spcs, names=names) for x in spcs]
+             result_dict["reactions"] = [obj_to_dict(x, spcs, names=names) for x in rxns]
+
      return result_dict
-    #  else:
-    #     result_dict = dict()
-    #     result_dict["Phases"] = [dict(),dict()]
-    #     result_dict["Phases"][0]["name"] = "gas"
-    #     result_dict["Phases"][1]["name"] = "surface"
-    #     result_dict["Interfaces"] = [dict()]
-    #     result_dict["Phases"][0]["Species"] = [obj_to_dict(x,spcs,names=names) for x in spcs if not x.contains_surface_site()]
-    #     result_dict["Phases"][1]["Species"] = [obj_to_dict(x,spcs,names=names) for x in spcs if x.contains_surface_site()]
-    #     result_dict["Reactions"] = [obj_to_dict(x, spcs,names=names) for x in rxns]
-    #     if solvent_data:
-    #         result_dict["Solvents"] = [obj_to_dict(solvent_data, spcs, names=names, label=solvent)]
- 
-
-
-
-
-     
-     
-     
-     #return result_dict
-     
-    #  names = [x.label for x in spcs]
-    #  for i,name in enumerate(names): #fix duplicate names
-    #     if names.count(name) > 1:
-    #         names[i] += "-"+str(names.count(name))
-        
- 
- 
- 
- 
- 
- 
- 
-    # names = [x.label for x in spcs]
-    # for i,name in enumerate(names): #fix duplicate names
-    #     if names.count(name) > 1:
-    #         names[i] += "-"+str(names.count(name))
-
-    # is_surface = False
-    # for spc in spcs:
-    #     if spc.contains_surface_site():
-    #         is_surface = True
-    #         break
-    # if not is_surface:
-    #     result_dict = dict()
-    #     result_dict["Units"] = dict()
-    #     result_dict["Phases"] = [dict()]
-    #     #result_dict["Phases"][0]["name"] = "phase"  #why is this here if it is redefined below?
-    #     result_dict["Phases"][0]["Species"] = [obj_to_dict(x, spcs, names=names) for x in spcs]
-    #     result_dict["Reactions"] = [obj_to_dict(x, spcs, names=names) for x in rxns]
-    #     if solvent_data:
-    #         result_dict["Solvents"] = [obj_to_dict(solvent_data, spcs, names=names, label=solvent)]
-    #     return result_dict
-    # else:
-    #     result_dict = dict()
-    #     result_dict["Units"] = dict()
-    #     result_dict["Phases"] = [dict(),dict()]
-    #     result_dict["Phases"][0]["name"] = "gas"
-    #     result_dict["Phases"][1]["name"] = "surface"
-    #     result_dict["Interfaces"] = [dict()]
-    #     result_dict["Phases"][0]["Species"] = [obj_to_dict(x,spcs,names=names) for x in spcs if not x.contains_surface_site()]
-    #     result_dict["Phases"][1]["Species"] = [obj_to_dict(x,spcs,names=names) for x in spcs if x.contains_surface_site()]
-    #     result_dict["Reactions"] = [obj_to_dict(x, spcs,names=names) for x in rxns]
-    #     if solvent_data:
-    #         result_dict["Solvents"] = [obj_to_dict(solvent_data, spcs, names=names, label=solvent)]
-    
 
 
 def obj_to_dict(obj, spcs, names=None, label="solvent"):
+
     result_dict = dict()
+
     if isinstance(obj, Species):
-        result_dict["name"] = obj.to_chemkin()
-        result_dict["composition"] = obj.to_cantera().composition
-        #result_dict["thermo"] = dict()
-        result_dict["thermo"] = obj_to_dict(obj.thermo, spcs)
+        s  = obj.to_cantera()
+        species_data = s.input_data
+        result_dict['note'] = obj.transport_data.comment
+        species_data.update(result_dict)
+        return species_data #returns composition, name, thermo, and transport, and note
+
+    if isinstance(obj, Reaction):
+         #doesn't work for fall-off reactions :( will have to do something else for that 
+        try: 
+            s  = obj.to_cantera()
+            reacton_data = s.input_data
+            return reacton_data
+        except: 
+            if isinstance(obj.kinetics, Troe) or isinstance(obj.kinetics, Lindemann): 
+                print('********veryyyyy interesting')
+                #result_dict
+            else:
+                print('********passing**********')
+            return result_dict
+            
+        #doesn't work for fall-off reactions :( will have to do something else for that 
+
+        # if str(s._reaction_type) != 'elementary':
+        #     result_dict['type'] = s._reaction_type
+        #     if s.duplicate == True: 
+        #         result_dict['duplicate'] = 'true'
+            
 
 
-        #result_dict["type"] = "Species"
-    #     if obj.contains_surface_site():
-    #         result_dict["adjlist"] = obj.molecule[0].to_adjacency_list()
-    #         result_dict["smiles"] = obj.molecule[0].to_smiles()
-    #         result_dict["thermo"] = obj_to_dict(obj.thermo, spcs)
-    #         result_dict["radicalelectrons"] = get_radicals(obj)
-    #     if obj.liquid_volumetric_mass_transfer_coefficient_data:
-    #         result_dict["liquidvolumetricmasstransfercoefficient"] = dict()
-    #         result_dict["liquidvolumetricmasstransfercoefficient"]["type"] = "TemperatureDependentLiquidVolumetricMassTransferCoefficient"
-    #         result_dict["liquidvolumetricmasstransfercoefficient"]["Ts"] = obj.liquid_volumetric_mass_transfer_coefficient_data.Ts
-    #         result_dict["liquidvolumetricmasstransfercoefficient"]["kLAs"] = obj.liquid_volumetric_mass_transfer_coefficient_data.kLAs
-    #     if obj.henry_law_constant_data:
-    #         result_dict["henrylawconstant"] = dict()
-    #         result_dict["henrylawconstant"]["type"] = "TemperatureDependentHenryLawConstant"
-    #         result_dict["henrylawconstant"]["Ts"] = obj.henry_law_constant_data.Ts
-    #         result_dict["henrylawconstant"]["kHs"] = obj.henry_law_constant_data.kHs
-    #elif isinstance(obj, NASA):
-    #     result_dict["model"] = [obj_to_dict(k, spcs) for k in obj.polynomials]
-    #     result_dict["type"] = "NASA"
-    elif isinstance(obj, NASAPolynomial):
-         result_dict["model"] = "NASApolynomial"
-         result_dict["data"] = obj.coeffs.tolist()
-    #     result_dict["Tmax"] = obj.Tmax.value_si
-    #     result_dict["Tmin"] = obj.Tmin.value_si
-    # elif isinstance(obj, Reaction):
-    #     result_dict["reactants"] = [names[spcs.index(x)] for x in obj.reactants]
-    #     result_dict["products"] = [names[spcs.index(x)] for x in obj.products]
-    #     result_dict["kinetics"] = obj_to_dict(obj.kinetics, spcs, names)
-    #     result_dict["type"] = "ElementaryReaction"
-    #     result_dict["radicalchange"] = sum([get_radicals(x) for x in obj.products]) - \
-    #                                    sum([get_radicals(x) for x in obj.reactants])
-    # elif isinstance(obj, Arrhenius):
-    #     obj.change_t0(1.0)
-    #     result_dict["type"] = "Arrhenius"
-    #     result_dict["A"] = obj.A.value_si
-    #     result_dict["Ea"] = obj.Ea.value_si
-    #     result_dict["n"] = obj.n.value_si
-    # elif isinstance(obj, StickingCoefficient):
-    #     obj.change_t0(1.0)
-    #     result_dict["type"] = "StickingCoefficient"
-    #     result_dict["A"] = obj.A.value_si
-    #     result_dict["Ea"] = obj.Ea.value_si
-    #     result_dict["n"] = obj.n.value_si
-    # elif isinstance(obj, PDepArrhenius):
-    #     result_dict["type"] = "PdepArrhenius"
-    #     result_dict["Ps"] = obj.pressures.value_si.tolist()
-    #     result_dict["arrs"] = [obj_to_dict(x, spcs) for x in obj.arrhenius]
-    # elif isinstance(obj, MultiArrhenius):
-    #     result_dict["type"] = "MultiArrhenius"
-    #     result_dict["arrs"] = [obj_to_dict(x, spcs) for x in obj.arrhenius]
-    # elif isinstance(obj, MultiPDepArrhenius):
-    #     result_dict["type"] = "MultiPdepArrhenius"
-    #     result_dict["parrs"] = [obj_to_dict(x, spcs) for x in obj.arrhenius]
-    # elif isinstance(obj, ThirdBody):
-    #     result_dict["type"] = "ThirdBody"
-    #     result_dict["arr"] = obj_to_dict(obj.arrheniusLow, spcs)
-    #     result_dict["efficiencies"] = {spcs[i].label: float(val)
-    #                                    for i, val in enumerate(obj.get_effective_collider_efficiencies(spcs)) if val != 1}
-    # elif isinstance(obj, Lindemann):
-    #     result_dict["type"] = "Lindemann"
-    #     result_dict["arrhigh"] = obj_to_dict(obj.arrheniusHigh, spcs)
-    #     result_dict["arrlow"] = obj_to_dict(obj.arrheniusLow, spcs)
-    #     result_dict["efficiencies"] = {spcs[i].label: float(val)
-    #                                    for i, val in enumerate(obj.get_effective_collider_efficiencies(spcs)) if val != 1}
+
+
+
     # elif isinstance(obj, Troe):
     #     result_dict["type"] = "Troe"
     #     result_dict["arrhigh"] = obj_to_dict(obj.arrheniusHigh, spcs)
@@ -313,7 +211,7 @@ def obj_to_dict(obj, spcs, names=None, label="solvent"):
     # else:
     #     raise ValueError("Object of type {} does not have a defined conversion to "
     #                      "ReactionMechanismSimulator format".format(type(obj)))
-    return result_dict
+  # return result_dict
 
 
 
